c99a2e203631014aa5891153160ba47ab930835b
sean <lyonssp777@gmail.com>
2020-12-13 11:20:14 -0600

    gofmt

--- set/set.go
+++ set/set.go
@@ -1,37 +1,37 @@
 package set
 
 import (
-    "fmt"
-    "github.com/syndtr/goleveldb/leveldb"
+	"fmt"
+	"github.com/syndtr/goleveldb/leveldb"
 )
 
 type Set struct {
-    ns  []byte
-    ldb *leveldb.DB
+	ns  []byte
+	ldb *leveldb.DB
 }
 
 func (s *Set) Add(x []byte) error {
-    return s.ldb.Put(s.key(x), []byte{}, nil)
+	return s.ldb.Put(s.key(x), []byte{}, nil)
 }
 
 func (s *Set) Remove(x []byte) error {
-    return s.ldb.Delete(s.key(x), nil)
+	return s.ldb.Delete(s.key(x), nil)
 }
 
 func (s *Set) Contains(x []byte) (bool, error) {
-    _, err := s.ldb.Get(s.key(x), nil)
-    if err != nil {
-        if err == leveldb.ErrNotFound {
-            return false, nil
-        }
-        return false, fmt.Errorf("leveldb get: %v", err)
-    }
-    return true, nil
+	_, err := s.ldb.Get(s.key(x), nil)
+	if err != nil {
+		if err == leveldb.ErrNotFound {
+			return false, nil
+		}
+		return false, fmt.Errorf("leveldb get: %v", err)
+	}
+	return true, nil
 }
 
 func (s *Set) key(x []byte) []byte {
-    namespaced := make([]byte, len(x) + len(s.ns))
-    copy(namespaced[:len(s.ns)], s.ns)
-    copy(namespaced[len(s.ns):], x)
-    return namespaced
+	namespaced := make([]byte, len(x)+len(s.ns))
+	copy(namespaced[:len(s.ns)], s.ns)
+	copy(namespaced[len(s.ns):], x)
+	return namespaced
 }
--- set/set_test.go
+++ set/set_test.go
@@ -1,37 +1,37 @@
 package set
 
 import (
-    "github.com/syndtr/goleveldb/leveldb"
-    "github.com/stretchr/testify/assert"
-    "io/ioutil"
-    "testing"
+	"github.com/stretchr/testify/assert"
+	"github.com/syndtr/goleveldb/leveldb"
+	"io/ioutil"
+	"testing"
 )
 
 func TestSet(t *testing.T) {
-    assert := assert.New(t)
+	assert := assert.New(t)
 
-    dir, err := ioutil.TempDir("", "test")
-    assert.Nil(err)
+	dir, err := ioutil.TempDir("", "test")
+	assert.Nil(err)
 
-    db, err := leveldb.OpenFile(dir, nil)
-    assert.Nil(err)
+	db, err := leveldb.OpenFile(dir, nil)
+	assert.Nil(err)
 
-    s := Set{
-        ns: []byte("xxx"),
-        ldb: db,
-    }
+	s := Set{
+		ns:  []byte("xxx"),
+		ldb: db,
+	}
 
-    err = s.Add([]byte("foo"))
-    assert.Nil(err)
+	err = s.Add([]byte("foo"))
+	assert.Nil(err)
 
-    contains, err := s.Contains([]byte("foo"))
-    assert.Nil(err)
-    assert.True(contains)
+	contains, err := s.Contains([]byte("foo"))
+	assert.Nil(err)
+	assert.True(contains)
 
-    err = s.Remove([]byte("foo"))
-    assert.Nil(err)
+	err = s.Remove([]byte("foo"))
+	assert.Nil(err)
 
-    contains, err = s.Contains([]byte("foo"))
-    assert.Nil(err)
-    assert.False(contains)
+	contains, err = s.Contains([]byte("foo"))
+	assert.Nil(err)
+	assert.False(contains)
 }
