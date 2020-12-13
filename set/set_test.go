00fb6995ad673687745d540d0b1ee37a49a39259
sean <lyonssp777@gmail.com>
2020-12-13 11:39:17 -0600

    add tests for multiple sets on one db

--- set/set_test.go
+++ set/set_test.go
@@ -35,3 +35,64 @@ func TestSet(t *testing.T) {
 	assert.Nil(err)
 	assert.False(contains)
 }
+
+func TestNamespacing(t *testing.T) {
+	t.Run("Add", func(t *testing.T) {
+		assert := assert.New(t)
+
+		dir, err := ioutil.TempDir("", "test")
+		assert.Nil(err)
+
+		db, err := leveldb.OpenFile(dir, nil)
+		assert.Nil(err)
+
+		a := Set{
+			ns:  []byte("xxx"),
+			ldb: db,
+		}
+		b := Set{
+			ns:  []byte("yyy"),
+			ldb: db,
+		}
+
+		err = a.Add([]byte("foo"))
+		assert.Nil(err)
+
+		contains, err := a.Contains([]byte("foo"))
+		assert.Nil(err)
+		assert.True(contains)
+
+		contains, err = b.Contains([]byte("foo"))
+		assert.Nil(err)
+		assert.False(contains)
+	})
+
+	t.Run("Remove", func(t *testing.T) {
+		assert := assert.New(t)
+
+		dir, err := ioutil.TempDir("", "test")
+		assert.Nil(err)
+
+		db, err := leveldb.OpenFile(dir, nil)
+		assert.Nil(err)
+
+		a := Set{
+			ns:  []byte("xxx"),
+			ldb: db,
+		}
+		b := Set{
+			ns:  []byte("yyy"),
+			ldb: db,
+		}
+
+		err = a.Add([]byte("foo"))
+		assert.Nil(err)
+
+		err = b.Remove([]byte("foo"))
+		assert.Nil(err)
+
+		contains, err := a.Contains([]byte("foo"))
+		assert.Nil(err)
+		assert.True(contains)
+	})
+}
